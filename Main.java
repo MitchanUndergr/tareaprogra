3d1ae0645dceed8882ea743b3784ec09a707d6fe
MitchanUndergr <131836786+MitchanUndergr@users.noreply.github.com>
2023-04-26 23:11:53 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -420,3 +420,21 @@ class Snickers extends Dulce {
         return b;
     }
 }
+
+class NoHayProductoException extends Exception{
+    public NoHayProductoException(String mensaje){
+        super(mensaje);
+    }
+}
+
+class PagoInsuficienteException extends Exception{
+    public  PagoInsuficienteException(String mensaje){
+        super(mensaje);
+    }
+}
+
+class PagoIncorrectoException extends  Exception{
+    public PagoIncorrectoException(String mensaje){
+        super(mensaje);
+    }
+}
